bc5f9e0172a75f371e8055b9bd3425bdf6cb81f8
veenith123 <72656950+veenith123@users.noreply.github.com>
2021-05-14 09:08:56 +0530

    hi

---
+++ Chapters.htmls/A greedy fat old man/part1/agfoma/agfomq/js/questions.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When did th old man get up?",
+    answer: "A. 6am",
+    options: [
+      "A. 6am",
+      "B. 7am"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When did he brush his teeth?",
+    answer: "B. 6.30am",
+    options: [
+      "A. 8.30am",
+      "B. 6.30am"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When did he take tea?",
+    answer: "A. 7am",
+    options: [
+      "A. 7am",
+      "B. 6.30am"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When did he take breakfast?",
+    answer: "A. 8.30am",
+    options: [
+      "A. 8.30am",
+      "B. 7am"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who did get up at 6am?",
+    answer: "B. The greedy fat old man",
+    options: [
+      "A. The little thin boy",
+      "B. The greedy fat old man"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ Chapters.htmls/A greedy fat old man/part2/part1q/js/questions.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who met a small dog?",
+    answer: "A. Old man",
+    options: [
+      "A. Old man",
+      "B. Little boy"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who asked many questions to the old man?",
+    answer: "B. Small dog",
+    options: [
+      "A. Small cat",
+      "B. Small dog"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who lost his temper?",
+    answer: "A. Old man",
+    options: [
+      "A. Old man",
+      "B. Small dog"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the past form of get up?",
+    answer: "B. Got up",
+    options: [
+      "A. Get's up",
+      "B. Got up"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the third person singular form of word 'take'?",
+    answer: "B. Took",
+    options: [
+      "A. Takes",
+      "B. Took"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
